129cd5dd4f416cd15718ebeecf51535a46322d0d
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-08-22 17:13:49 +0200

    Create controller.c

---
+++ orbita3d_c_api/c-examples/controller.c
@@ -0,0 +1,74 @@
+#include "orbita3d.h"
+#include <math.h>
+
+#define DEG2RAD (M_PI / 180.0)
+#define RAD2DEG (180.0 / M_PI)
+
+// Compile using
+// g++ controller.c -I../ -L../../target/debug -lorbita3d_c_api -o controller
+// Run using
+// ./controller path_to_config_file
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s path_to_config_file\n", argv[0]);
+        return 1;
+    }
+
+    uint32_t uid;
+    if (orbita3d_controller_from_config(argv[1], &uid) != 0) {
+        return 1;
+    }
+    printf("Controller uid: %d\n", uid);
+
+    bool is_on;
+    if (orbita3d_is_torque_on(uid, &is_on) != 0) {
+        return 1;
+    }
+    printf("Torque is %s\n", is_on ? "on" : "off");
+
+    printf("Turning torque on\n");
+    if (orbita3d_enable_torque(uid, true) != 0) {
+        return 1;
+    }
+    if (orbita3d_is_torque_on(uid, &is_on) != 0) {
+        return 1;
+    }
+    printf("Torque is %s\n", is_on ? "on" : "off");
+
+    printf("Getting current orientation\n");
+    double q[4];
+    if (orbita3d_get_current_orientation(uid, &q) != 0) {
+        return 1;
+    }
+    printf("\tquat: %f %f %f %f\n", q[0], q[1], q[2], q[3]);
+
+    double rpy[3];
+    quaternion_to_intrinsic_roll_pitch_yaw(&q, &rpy);
+    printf("\trpy: %f %f %f\n", rpy[0], rpy[1], rpy[2]);
+
+    double target_rpy[3] = {0.0, 0.0, 0.25};
+    printf("Setting target orientation to %f %f %f\n", target_rpy[0], target_rpy[1], target_rpy[2]);
+    double target_q[4];
+    intrinsic_roll_pitch_yaw_to_quaternion(&target_rpy, &target_q);
+    if (orbita3d_set_target_orientation(uid, &target_q) != 0) {
+        return 1;
+    }
+
+    printf("Getting target orientation\n");
+    if (orbita3d_get_target_orientation(uid, &q) != 0) {
+        return 1;
+    }
+    quaternion_to_intrinsic_roll_pitch_yaw(&q, &rpy);
+
+    printf("\trpy: %f %f %f\n", rpy[0], rpy[1], rpy[2]);
+
+    printf("Getting current orientation\n");
+    if (orbita3d_get_current_orientation(uid, &q) != 0) {
+        return 1;
+    }
+    quaternion_to_intrinsic_roll_pitch_yaw(&q, &rpy);
+    printf("\trpy: %f %f %f\n", rpy[0], rpy[1], rpy[2]);
+
+    return 0;
+}
