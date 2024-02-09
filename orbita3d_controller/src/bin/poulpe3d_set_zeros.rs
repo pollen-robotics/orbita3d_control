0d107504d0c6a73d0649ea6a7951ae4701778133
Steve Nguyen <steve.nguyen@pollen-robotics.com>
2024-02-09 21:15:17 +0100

    wip zero

--- orbita3d_controller/src/bin/poulpe3d_set_zeros.rs
+++ orbita3d_controller/src/bin/poulpe3d_set_zeros.rs
@@ -4,8 +4,8 @@ use std::f64::consts::PI;
 use std::time::SystemTime;
 use std::{error::Error, thread, time::Duration, time::Instant};
 
-use log::{info, warn};
 use log::Level;
+use log::{info, warn};
 
 use clap::Parser;
 
@@ -15,9 +15,7 @@ struct Args {
     /// tty
     #[arg(short, long, default_value = "config/dxl_poulpe.yaml")]
     configfile: String,
-
 }
-
 
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
@@ -25,34 +23,35 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     log::info!("Config file: {}", args.configfile);
 
-
     let mut controller = Orbita3dController::with_config(&args.configfile)?;
 
-    let t=controller.is_torque_on();
+    let t = controller.is_torque_on();
     match t {
-		Ok(t) => log::info!("Torque is {}", t),
-		Err(e) => log::error!("Error: {}", e),
-	}
+        Ok(t) => log::info!("Torque is {}", t),
+        Err(e) => log::error!("Error: {}", e),
+    }
     thread::sleep(Duration::from_millis(10));
 
-    let r=controller.disable_torque();
-	match r {
-		Ok(_) => log::info!("Torque is off"),
-		Err(e) => log::error!("Error: {}", e),
-	}
+    let r = controller.disable_torque();
+    match r {
+        Ok(_) => log::info!("Torque is off"),
+        Err(e) => log::error!("Error: {}", e),
+    }
     thread::sleep(Duration::from_millis(1000));
-    warn!("Set Orbita3D in the zero position!");
-    thread::sleep(Duration::from_secs(10));
-    let axis_sensors=controller.get_axis_sensors()?;
+    println!("Set Orbita3D in the zero position! (5s)");
+    thread::sleep(Duration::from_secs(5));
+    let mut axis_sensors = controller.get_axis_sensors()?;
 
+    for s in axis_sensors.iter_mut() {
+        *s *= 12.0 / 64.0; //FIXME how to get the ratio?
+    }
 
     println!("Zeros to write to the yaml config");
     println!();
     println!("disks:");
     println!("  zeros: !HallZero");
     println!("    hardware_zero: {:?}", axis_sensors);
-    println!("    hall_indice: [0, 5, 10] (FIXME: This should be known from the hardware)");
-
+    println!("    hall_indice: [0, 5, 11] (FIXME: This should be known from the hardware)");
 
     Ok(())
 }
--- orbita3d_controller/src/io/cached_poulpe.rs
+++ orbita3d_controller/src/io/cached_poulpe.rs
@@ -20,7 +20,6 @@ pub struct CachedDynamixelPoulpeController {
     current_position: Cache<u8, [f64; 3]>,
     current_velocity: Cache<u8, [f64; 3]>,
     current_torque: Cache<u8, [f64; 3]>,
-
 }
 
 impl CachedDynamixelPoulpeController {
@@ -34,15 +33,12 @@ impl CachedDynamixelPoulpeController {
             torque_limit: Cache::keep_last(),
             pid_gains: Cache::keep_last(),
 
-	    //Or expiration time?
-	    current_position: Cache::keep_last(),
-	    current_velocity: Cache::keep_last(),
-	    current_torque: Cache::keep_last(),
-
+            //Or expiration time?
+            current_position: Cache::keep_last(),
+            current_velocity: Cache::keep_last(),
+            current_torque: Cache::keep_last(),
         })
     }
-
-
 }
 
 impl MotorsController<3> for CachedDynamixelPoulpeController {
@@ -106,22 +102,21 @@ impl RawMotorsIO<3> for CachedDynamixelPoulpeController {
             .or_try_insert_with(|_| RawMotorsIO::get_target_position(&mut self.inner))
     }
     fn set_target_position(&mut self, position: [f64; 3]) -> Result<()> {
-
         // let current_position = RawMotorsIO::get_target_position(self)?;
-	//we at least always write the target
+        //we at least always write the target
         // if current_position != position {
-            let fb=RawMotorsIO::set_target_position_fb(&mut self.inner, position)?;
+        let fb = RawMotorsIO::set_target_position_fb(&mut self.inner, position)?;
 
-            self.target_position.insert(self.inner.id(), position);
-	    let cpos:[f64;3]=[fb[0],fb[1],fb[2]];
-	    // let cpos:[f64;3]=[0.0;3];
-	    // cpos.clone_from_slice(&fb[0..3]);
-	    let cvel:[f64;3]=[fb[3],fb[4],fb[5]];
-	    let ctorque:[f64;3]=[fb[6],fb[7],fb[8]];
+        self.target_position.insert(self.inner.id(), position);
+        let cpos: [f64; 3] = [fb[0], fb[1], fb[2]];
+        // let cpos:[f64;3]=[0.0;3];
+        // cpos.clone_from_slice(&fb[0..3]);
+        let cvel: [f64; 3] = [fb[3], fb[4], fb[5]];
+        let ctorque: [f64; 3] = [fb[6], fb[7], fb[8]];
 
-	    self.current_position.insert(self.inner.id(), cpos); //I would like to be able to use a slice here &fb[0..3] but it doesn't work
-	    self.current_velocity.insert(self.inner.id(), cvel);
-	    self.current_torque.insert(self.inner.id(), ctorque);
+        self.current_position.insert(self.inner.id(), cpos); //I would like to be able to use a slice here &fb[0..3] but it doesn't work
+        self.current_velocity.insert(self.inner.id(), cvel);
+        self.current_torque.insert(self.inner.id(), ctorque);
 
         // }
 
@@ -131,27 +126,26 @@ impl RawMotorsIO<3> for CachedDynamixelPoulpeController {
     fn set_target_position_fb(&mut self, position: [f64; 3]) -> Result<[f64; 9]> {
         // let current_position = RawMotorsIO::get_target_position(self)?;
 
-	// let mut fb:[f64;9]=[0.0;9];
-	//we at least always write the target
+        // let mut fb:[f64;9]=[0.0;9];
+        //we at least always write the target
         // if current_position != position {
-            let fb=RawMotorsIO::set_target_position_fb(&mut self.inner, position)?;
+        let fb = RawMotorsIO::set_target_position_fb(&mut self.inner, position)?;
 
-            self.target_position.insert(self.inner.id(), position);
-	    let cpos:[f64;3]=[fb[0],fb[1],fb[2]];
-	    // let cpos:[f64;3]=[0.0;3];
-	    // cpos.clone_from_slice(&fb[0..3]);
-	    let cvel:[f64;3]=[fb[3],fb[4],fb[5]];
-	    let ctorque:[f64;3]=[fb[6],fb[7],fb[8]];
+        self.target_position.insert(self.inner.id(), position);
+        let cpos: [f64; 3] = [fb[0], fb[1], fb[2]];
+        // let cpos:[f64;3]=[0.0;3];
+        // cpos.clone_from_slice(&fb[0..3]);
+        let cvel: [f64; 3] = [fb[3], fb[4], fb[5]];
+        let ctorque: [f64; 3] = [fb[6], fb[7], fb[8]];
 
-	    self.current_position.insert(self.inner.id(), cpos); //I would like to be able to use a slice here &fb[0..3] but it doesn't work
-	    self.current_velocity.insert(self.inner.id(), cvel);
-	    self.current_torque.insert(self.inner.id(), ctorque);
+        self.current_position.insert(self.inner.id(), cpos); //I would like to be able to use a slice here &fb[0..3] but it doesn't work
+        self.current_velocity.insert(self.inner.id(), cvel);
+        self.current_torque.insert(self.inner.id(), ctorque);
 
         // }
 
         Ok(fb)
     }
-
 
     fn get_velocity_limit(&mut self) -> Result<[f64; 3]> {
         self.velocity_limit
@@ -203,10 +197,7 @@ impl RawMotorsIO<3> for CachedDynamixelPoulpeController {
 
         Ok(())
     }
-    fn get_axis_sensors(&mut self) -> Result<[f64;3]>
-    {
-	RawMotorsIO::get_axis_sensors(&mut self.inner)
-
+    fn get_axis_sensors(&mut self) -> Result<[f64; 3]> {
+        RawMotorsIO::get_axis_sensors(&mut self.inner)
     }
-
 }
